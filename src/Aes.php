6cee673c1ce8f8d04842949559a3d6a022299eb5
easy-produce <Anonymity@gmail.com>
2022-08-17 16:09:57 +0800

    对AES增加扩展

--- src/Aes.php
+++ src/Aes.php
@@ -9,16 +9,16 @@ class Aes
      * DES-ECB DES-CBC DES-CTR DES-OFB DES-CFB
      * $data, $method, $key, $options = 0,
      */
-    public static function encode(string $data, string $method, string $aesKey, int $options = OPENSSL_RAW_DATA): string
+    public static function encode(string $data, string $method, string $aesKey, int $options = OPENSSL_RAW_DATA, string $iv = "", string $tag = "", string $aad = "", int $tag_length = 16): string
     {
-        return base64_encode(openssl_encrypt($data, $method, $aesKey, $options));
+        return base64_encode(openssl_encrypt($data, $method, $aesKey, $options, $iv, $tag, $aad, $tag_length));
     }
 
     /**
      * 解密
      */
-    public static function decode(string $data, string $method, string $aesKey, int $options = OPENSSL_RAW_DATA): string
+    public static function decode(string $data, string $method, string $aesKey, int $options = OPENSSL_RAW_DATA, string $iv = "", string $tag = "", string $aad = ""): string
     {
-        return openssl_decrypt(base64_decode($data), $method, $aesKey, $options);
+        return openssl_decrypt(base64_decode($data), $method, $aesKey, $options, $iv, $tag, $aad);
     }
 }
